90f505d8984454fc7eb9d8c8a315c8b20b762bdd
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-08-27 09:45:07 +0100

    Initial Commit

---
+++ Shared/CitySightsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsAppApp.swift
+//  Shared
+//
+//  Created by Shamsuddin Refaei on 27/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
